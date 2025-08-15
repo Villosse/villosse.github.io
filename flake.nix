22076467ab130d90ae93f1951b553fc6a7a1045c
lenny.chiadmi-delage <lenny.chiadmi-delage@epita.fr>
2025-08-15 19:21:47 +0200

    feat: flakes for dev

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+    description = "My portfolio website";
+    inputs = {
+nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    futils.url = "github:numtide/flake-utils";
+
+    };
+
+    outputs = {
+    self,
+    nixpkgs,
+    futils,
+    }@inputs:
+    let
+        inherit (nixpkgs) lib;
+        inherit (lib) recursiveUpdate;
+        inherit (futils.lib) eachDefaultSystem defaultSystems;
+
+        nixpkgsFor = lib.genAttrs defaultSystems (system: import nixpkgs {
+            inherit system;
+        }
+        );
+        in
+        (
+        eachDefaultSystem (
+              system:
+              let
+                pkgs = nixpkgsFor.${system};
+              in
+              {
+                devShell = pkgs.mkShell {
+                  buildInputs = with pkgs; [
+                    git
+                    pre-commit
+                    nodejs_24
+                    yarn
+                    zip
+                  ];
+                  shellHook = ''
+                    #Install dependencies
+                    yarn install --prod
+                  '';
+                };
+                }
+                ));
+}
