067ea596f175774d544a5d22121b4f3adc029895
lenny.chiadmi-delage <lenny.chiadmi-delage@epita.fr>
2025-11-20 18:38:35 +0100

    fix: deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    base: '/',
+    base: process.env.VITE_BASE_PATH || '/',
     assetsInclude: ['**/*.yml', '**/*.yaml'],
     resolve: {
       alias: {
